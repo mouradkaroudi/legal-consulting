d539fc0ac5abd545acf6c1ef833d71f61c70a6d4
unknown <mkaroudi@gmail.com>
2022-11-21 22:44:36 +0100

    wip

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
 	use HasFactory, Notifiable, HasRoles;
 

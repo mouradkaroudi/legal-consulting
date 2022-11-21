8b8bc932473d4f6834ff272b29462f08859b9737
unknown <mkaroudi@gmail.com>
2022-11-21 22:45:15 +0100

    wip

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -29,7 +29,7 @@ class Admin extends Authenticatable implements FilamentUser
 	/**
 	 *
 	 */
-	public function canAccessFilament()
+	public function canAccessFilament():bool
 	{
 		return true;
 	}

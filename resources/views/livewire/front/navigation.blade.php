585cb982f50557ce08db484526e50b16716a5ea2
unknown <mkaroudi@gmail.com>
2022-12-15 19:58:19 +0100

    wip

--- resources/views/livewire/front/navigation.blade.php
+++ resources/views/livewire/front/navigation.blade.php
@@ -28,7 +28,8 @@
                         class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                         <div class="py-1" role="none">
                             @foreach($menuItem['children'] as $meniChildItem)
-                            <a href="{{ $meniChildItem['data']['url'] }}" class="block py-2 px-4 hover:bg-gray-100" aria-current="page">
+
+                            <a href="{{ $meniChildItem['data'] ? $meniChildItem['data']['url'] : ''  }}" class="block py-2 px-4 hover:bg-gray-100" aria-current="page">
                                 {{ $meniChildItem['label'] }}
                             </a>
                             @endforeach
